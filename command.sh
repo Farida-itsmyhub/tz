6ddd0f0c6bea4be7865614f64e26b011ec631f3e
Farida-itsmyhub <48317053+Farida-itsmyhub@users.noreply.github.com>
2021-06-08 14:40:51 +0300

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,4 +1,2 @@
-docker build -t tz .
-docker run -t -d --name tztz  tz
-docker start <conrainer_ID>
-docker exec -it <conrainer_ID> bash
+docker build -t <name_container> .
+docker run --rm -it -v <path to folder with images>:/usr/src/app/dataset -v {path to folder for out}:/usr/src/app/data_out <name_container>
